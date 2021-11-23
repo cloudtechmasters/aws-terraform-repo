dfcd1f0a70393427fbc3fd27013801bf9b0d42f3
cloudtechmasters <68885738+cloudtechmasters@users.noreply.github.com>
2021-11-23 13:19:30 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -10,6 +10,4 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  shared_credentials_file = "C:\\Users\\SRINADH\\.aws\\credentials"
-  profile                 = "aws-devops-9pm"
 }
