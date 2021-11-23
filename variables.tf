8efeb643cba263222812801a81700653ecfa5b3e
cloudtechmasters <68885738+cloudtechmasters@users.noreply.github.com>
2021-11-23 13:37:32 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "access_key" {
+    description = "The access_key where to provision resources"
+    type = string
+}
+variable "secret_key" {
+    description = "The secret_key where to provision resources"
+    type = string
+}
